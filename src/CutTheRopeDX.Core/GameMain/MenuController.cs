850d965cf6ea3848b959fe9b67202312dd2c027f
CtRHome <231761651+CtRHome@users.noreply.github.com>
2026-09-11 12:06:40 -0300

    stuff

--- src/CutTheRopeDX.Core/GameMain/MenuController.cs
+++ src/CutTheRopeDX.Core/GameMain/MenuController.cs
@@ -379,7 +379,13 @@ namespace CutTheRopeDX.GameMain
                         break;
                 }
 
-                _ = logoParent.AddChild(logo);
+                _ = logoParent.AddChild(image3);
+                Image imagePackage = Image.Image_createWithResIDQuad(Resources.Img.CommunityPackageSubtitle, 0);
+                imagePackage.anchor = 10;
+                imagePackage.parentAnchor = 10;
+                imagePackage.y = 500f;
+                imagePackage.x = 20f;
+                _ = logoParent.AddChild(imagePackage);
 
             }
             if (s)
